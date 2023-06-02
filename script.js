b7c80486a3d0c6f2fdaa3a08eb0b01946ca35191
Brandon Rimes <brimes@efe1963.com>
2023-06-02 17:55:33 -0400

    update

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const menuButton = document.getElementById("menuButton");
-const menuLine = document.getElementById("menuLine");
-const menuLineBefore = window.getComputedStyle(menuLine,"::before");
-const menuLineAfter = window.getComputedStyle(menuLine,"::after");
+const menuLineTop = document.getElementById("menuLineTop");
+const menuLineCenter = document.getElementById("menuLineCenter");
+const menuLineBottom = document.getElementById("menuLineBottom");
+
+let menuOpen = false;
+
 menuButton.addEventListener("click", function() {
-  menuLineBefore.style.backgroundColor = "red";
-  console.log("word");
+  
+  menuOpen ? menuOpen = false : menuOpen = true;
+
+  if (menuOpen) {
+    menuLineTop.style.transform = "rotate(0deg) translateY(0rem) translateX(0)";
+    menuLineBottom.style.transform = "rotate(0deg) translateY(0) translateX(0)";
+    menuLineCenter.style.width = "100%";
+  }
+
+  if (!menuOpen) {
+    menuLineTop.style.transform = "rotate(45deg) translateY(0.75rem) translateX(0.5rem)";
+    menuLineBottom.style.transform = "rotate(-45deg) translateY(-0.75rem) translateX(0.5rem)";
+    menuLineCenter.style.width = "0";
+  }
 });
