a3e3f2d3d19156086c5b01d436f0bdca0edfd357
pajareshd <printquestllc@gmail.com>
2023-06-02 16:08:29 -0400

    bert log 1

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const menuButton = document.getElementById("menuButton");
 const menuLine = document.getElementById("menuLine");
-
-menuLine.addEventListener("click", function() {
-  menuLine.style.backgroundColor = "red";
+const menuLineBefore = window.getComputedStyle(menuLine,"::before");
+const menuLineAfter = window.getComputedStyle(menuLine,"::after");
+menuButton.addEventListener("click", function() {
+  menuLineBefore.style.backgroundColor = "red";
+  console.log("word");
 });
